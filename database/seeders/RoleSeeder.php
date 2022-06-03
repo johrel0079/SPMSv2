f7fcb1ac779b58d4f1ade9c9b3a262777e2c3c17
johrel0079 <johrel.manzano0079@gmail.com>
2022-06-03 14:00:40 +0800

    area: add seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = [
+            'Admin', 
+            'Picker',
+            'Checker',
+            'Receiving',
+            'Guest'
+          ];
+  
+          for($i=0; $i<$role.count(); $i++){
+              Role::create([
+                  'role'  => $role[$i],
+              ]);
+          }
+    }
+}
