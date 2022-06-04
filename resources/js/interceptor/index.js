b430251346eed8f831b5aec8c7cf80bb5e3671a3
johrel0079 <johrel.manzano0079@gmail.com>
2022-06-04 10:03:27 +0800

    config: add interceptor

---
+++ resources/js/interceptor/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import store from '../store';
+
+
+export default function setupInterceptor() {
+    axios.interceptors.request.use(request => {
+        (config) => {
+            let token = store.state.auth.token;
+            if(token){
+                config.headers['Authorization'] = `Bearer ${ token }`
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    });
+}
