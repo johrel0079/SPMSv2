a35cfc2cd412f7c738d135d0d0467101067523ad
Erika <erika.reformado@gmail.com>
2022-06-05 22:27:01 +0800

    area code: add mixins

--- resources/js/Pages/Management/AreaCode/Index.vue
+++ resources/js/Pages/Management/AreaCode/Index.vue
@@ -5,11 +5,9 @@
     <div class="mb-4">
          <div class="container px-3">
              <h5 class="mt-3">Area Management</h5> 
-              <b-row>
-                  <b-col sm="6">
                       <b-row>
                           <b-col sm="6">
-                              <div class="mt-3">
+                              <!-- <div class="mt-3"> -->
                                 <label for="area-code">Area code</label>
                                 <b-form-input
                                     id="area-code"
@@ -17,44 +15,39 @@
                                     placeholder="Area Code"
                                     required
                                 ></b-form-input>
-                                    <div class="mt-2">
-                                        <label>Status</label>
-                                        <div class="form-check form-switch">
-                                            <input class="form-check-input" v-model="status" type="checkbox" role="switch" id="status">
-                                        </div>
-                                    </div>
-                              </div>
-                              
+                            </b-col>
+                            <b-col sm="6">
+                                <label>Status</label>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" v-model="status" type="checkbox" role="switch" id="status">
+                                </div>
+                            </b-col>
+                            <div>
                                 <b-button variant="primary" size="sm" class="px-4 mt-3">
-                                    Save
-                                </b-button>
-                          </b-col>
+                                Save
+                            </b-button>
+                            </div>
                       </b-row>
-                       
-                  </b-col>
-                  <b-col sm="6">
-                    <b-card>
+
+                    <b-card class="mt-3">
 
                             <h6>List of Area Code</h6>
                                 <table-component    
                                     :fields="fields"
-                                    :items="items"
+                                    :items="area_code_list"
                                     :perPage="10"
                                     :rows="5"
                                     :status="''"
                                     :action_dropdown="true"
                                     :is_search="true"
                                     :flag="'area_code'"
-                                    :is_select= "false">
-                                    <template #action>
-                                        <button class="btn btn-success btn-md"><b-icon icon="pencil-square"></b-icon></button> 
-                                        <button class="btn btn-danger btn-md"><b-icon icon="trash-fill"></b-icon> </button>
-                                    </template>
+                                    :is_select= "false"
+                                    :getId="getAreaId()">
                                     </table-component>
                  
                     </b-card>
-                  </b-col>
-              </b-row>
+                 
+            
             </div>
     </div>
 
@@ -67,35 +60,40 @@
 </template>
 
 <script>
-
-
+import AreaCodeMixin from '../../../mixins/AreaCode'
 export default 
 {
+    mixins: [AreaCodeMixin],
     data(){
         return {
             area_code: '',
             status: null,
             fields: [
             {
-                key: 'area_code',
+                key: 'code',
                 sortable: true
             },
-            {
-                key: 'status',
-                sortable: false
-            },
+            // {
+            //     key: 'status',
+            //     sortable: false
+            // },
             {
                 key: 'action',
             }
             ],
-            items: [
-                {
-                    area_code: 'C1',
-                    status: 1
-                }
-            ],
+            area_code_list: [],
+            id: 0,
         };
        
+    },
+    mounted(){
+        this.$_areaCodeMixin_getList();
+    },
+    methods: {
+        getAreaId(id){
+            this.id = id;
+            console.log(id)
+        }
     }
 }
 </script>
