370643a0f5cb909fb46073fcd2f852f6823de401
Erika <erika.reformado@gmail.com>
2022-06-06 10:06:44 +0800

    config: add route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+Route::get('{any}', [PageController::class, 'index'])->where('any','.*');
 
 Route::get('/', function () {
     return view('auth.login');
