b2fdfe35f3be38c70cd9fe199a7f63c7bb19e16b
Erika <erika.reformado@gmail.com>
2022-06-06 10:07:08 +0800

    config: add path after login

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -10,53 +10,55 @@ import AreaCode from '../Pages/Management/AreaCode/Index'
 
 Vue.use(VueRouter);
 
-const routes = [
-	{
-		path: "/login",
-		name: "login",
-		component: Login ,
-		meta: {
-			title: 'Login',
-			
-		}
-	},
-	{
-			path: "/",
-			name: "dashboard",
-			component: Dashboard,
-			meta: {
-				title: 'Dashboard',
-				requiredAuth: true
-			}
-	},
-	{
-		path: "/area-code",
-		name: 'area-code',
-		component: AreaCode,
-		meta: {
-			title: 'Area Code',
-			requiredAuth: true
-		}
-	}
+const routes = [{
+        path: "/login",
+        name: "login",
+        component: Login,
+        meta: {
+            title: 'Login',
+
+        }
+    },
+    {
+        path: "/dashboard",
+        name: "dashboard",
+        component: Dashboard,
+        meta: {
+            title: 'Dashboard',
+            requiredAuth: true
+        }
+    },
+    {
+        path: "/area-code",
+        name: 'area-code',
+        component: AreaCode,
+        meta: {
+            title: 'Area Code',
+            requiredAuth: true
+        }
+    },
+    {
+        path: '*',
+        redirect: '/login'
+    }
 ]
 const router = new VueRouter({
-	mode: "hash",
-	base: process.env.BASE_URL,
-	routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 router.beforeEach((to, from, next) => {
-	
-	if (to.name=='login' && localStorage.getItem('is_authenticated')) {
-        next({name: 'dashboard'})
+
+    if (to.name == 'login' && localStorage.getItem('is_authenticated')) {
+        console.log('done')
+        next({ name: 'dashboard' })
+    } else if (to.matched.some(record => record.meta.requiredAuth) && !localStorage.getItem('is_authenticated')) {
+        console.log('yes')
+        next({ name: 'login' })
+    } else {
+        next();
     }
-	else if(to.matched.some(record => record.meta.requiredAuth) && !localStorage.getItem('is_authenticated'))
-	{
-		console.log('yes')
-		next({name: 'login'})
-	}else{
-		next();
-	}
 })
 
 export default router;
